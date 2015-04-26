0644a6cc2bb52a0eadd0c2820feee86c1fe3b9b9
shelly8525 <shelly8525@yahoo.com.tw>
2015-04-26 20:48:12 +0800

    寫一個深層複製的方法

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,6 +1,9 @@
 import java.util.ArrayList;
 
-public class MyStack {
+public class MyStack implements Cloneable{
+	public static void main(String[] args){
+		
+	} 
 	private ArrayList<Object> list = new ArrayList<Object>();
 	
 	public boolean isEmpty(){
@@ -28,5 +31,16 @@ public class MyStack {
 	@Override
 	public String toString(){
 		return "stack:" + list.toString();
+	}
+	
+	public Object clone(){
+		try{
+			MyStack MyStack1 = (MyStack)super.clone();
+			MyStack1.list = (ArrayList<Object>)(list.clone());
+			return MyStack1;
+		}
+		catch(CloneNotSupportedException ex){
+			return null;
+		}
 	}
 }
